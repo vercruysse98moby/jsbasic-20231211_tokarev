58c9e11ca702510cd95888c625fde9cdc66bda58
Mihail Tokarev <106751671+Tokarev-Mikhail@users.noreply.github.com>
2023-12-15 10:57:13 +0300

    решение задачи первого модуля

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,7 @@
-function factorial(n) {
-  // ваш код...
-}
+  function factorial(n) {
+      let result = n;
+      for (let i = n - 1; i > 1; --i) {
+          result *= i;
+      }
+      return result;
+  }
