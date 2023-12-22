ecd46ffcc8c62e7d50bfe0e41e97dc895610d4eb
Mihail Tokarev <106751671+Tokarev-Mikhail@users.noreply.github.com>
2023-12-22 12:23:33 +0300

    решение 2 задачи 2 модуля

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,12 @@
 function isEmpty(obj) {
-  // ваш код...
+  var counter = 0;
+  for (var key in obj) {
+      counter++;
+  }
+  if (counter == 0) {
+      return true;
+  }
+  else {
+      return false;
+  }
 }
