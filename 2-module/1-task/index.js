0429cbeb8e089b2a9edd6d7dd0d3ef10ca15251a
Mihail Tokarev <106751671+Tokarev-Mikhail@users.noreply.github.com>
2023-12-22 12:17:01 +0300

    решение 1 задачи 2 модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for(let key in salaries) {
+      if (typeof salaries[key] === "number" && !isNaN(salaries[key]) && salaries[key] != Infinity && salaries[key] != -Infinity)
+          sum += salaries[key];          
+  }
+  return sum;
 }
