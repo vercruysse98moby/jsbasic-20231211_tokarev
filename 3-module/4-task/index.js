f123a0baefdfcae51e23122a1e143d57170dab27
Mihail Tokarev <106751671+Tokarev-Mikhail@users.noreply.github.com>
2023-12-25 16:50:30 +0300

    решение 4 задачи 3 модуля

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = [];
+  users.forEach(function(item) {
+    if (item.age <= age){
+      result.push(`${item.name}, ${item.balance}`);
+    }
+  });
+ 
+  return result.join('\n');
 }
