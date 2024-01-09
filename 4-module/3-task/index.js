eeaf854ab64b09a617ea808eaa7cf1febf899ca8
Mihail Tokarev <106751671+Tokarev-Mikhail@users.noreply.github.com>
2024-01-09 11:29:07 +0300

    решение 2й задачи 4 модуля

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,30 @@
 function highlight(table) {
-  // ваш код...
+  for (let tr of table.children[1].rows) { 
+      let available = tr.cells[3];
+      if (available.hasAttribute("data-available"))
+          switch (available.dataset.available) {
+              case "true":
+                  tr.classList.add("available");
+                  break;
+              case "false":
+                  tr.classList.add("unavailable");
+          }
+      else  
+          tr.hidden = true;
+       
+      let gender = tr.cells[2].textContent;
+      switch (gender) {
+          case "m":
+              tr.classList.add("male");
+              break;
+          case "f":
+              tr.classList.add("female");
+      }
+     
+      let age = +tr.cells[1].textContent;
+      if (age < 18) tr.style.textDecoration = "line-through";
+  }
 }
+
+let table = document.querySelector(".js-teachers");
+highlight(table);
